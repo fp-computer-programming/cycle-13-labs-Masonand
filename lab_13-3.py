50121d18c6838c01377fd245b2954ac7bad7f6af
Masonand <144147805+Masonand@users.noreply.github.com>
2024-03-04 22:18:27 -0500

    Add files via upload

---
+++ lab_13-3.py
@@ -0,0 +1,12 @@
+
+#touple for weekdays
+weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+
+#for weekends
+weekends = ('Saturday', 'Sunday')
+
+#concentrate to days of the week
+days_of_the_week = weekdays + weekends
+
+#Printing days_of_the_week
+print("Days of the week:", days_of_the_week)
---
+++ lab_13-4.py
@@ -0,0 +1,13 @@
+
+#created a list with the number day for the next five days
+day_date = [2, 3, 4, 5, 6]  
+
+#assinging the 5 variables in the list to numbers of each day
+(thu, fri, mon, tue, wed) = day_date
+
+#printing each variable to see what they equal
+print("Thursday:", thu)
+print("Friday:", fri)
+print("Monday:", mon)
+print("Tuesday:", tue)
+print("Wednesday:", wed)
