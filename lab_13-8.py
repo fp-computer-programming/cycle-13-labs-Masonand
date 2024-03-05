47ba4a86bb17617ffc9d4e567c2f6ac35ea73892
Masonand <144147805+Masonand@users.noreply.github.com>
2024-03-04 22:34:23 -0500

    Add files via upload

---
+++ lab_13-8.py
@@ -0,0 +1,6 @@
+
+# Using the 'with' statement to open a file for writing
+with open('my_file.txt', 'w') as file:
+    file.write('Initials: MA, Date: 2024-03-04\n')
+    file.write('Hello World!\n')
+    file.write('Lately, for fun, I have been learning Python programming and exploring data science.\n')
