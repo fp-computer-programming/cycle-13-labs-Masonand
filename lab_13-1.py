c72199680b2067ebe31878670307915d5f9e1502
Masonand <144147805+Masonand@users.noreply.github.com>
2024-03-04 22:11:48 -0500

    Add files via upload

---
+++ lab_13-1.py
@@ -0,0 +1,18 @@
+
+# Creating a dictionary
+row = {}
+
+#Adding the key value pairs
+row[1] = 'Alice'
+row[2] = 'Bob'
+row[3] = 'Charlie'
+
+#Printing the dictionary
+print("Initial row:", row)
+
+
+#Imagine someone named 'Dave' sat at computer 2
+row[2] = 'Dave'
+
+#print updated directory
+print("Updated row:", row)
